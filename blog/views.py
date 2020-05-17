b0d36651d8d3b4a89be51a566b780dde01b32967
Jon Tran <jontran@Jons-iMac.wp.shawcable.net>
2020-05-17 18:45:45 -0500

    Add post detail route and template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import  Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 def home(request):
   context = {
@@ -17,6 +17,10 @@ class PostListView(ListView):
 def about(request):
   return render(request, 'blog/about.html', {'title': 'About'})
 
+
+class PostDetailView(DetailView):
+  model = Post  
+  
 
 
 
