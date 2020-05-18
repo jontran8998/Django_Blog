d0580197906815adabad22377371cb90ac71cce3
Jon Tran <jontran@Jons-iMac.wp.shawcable.net>
2020-05-18 10:04:02 -0500

    Feature_Pagination on Posts home apge

--- blog/views.py
+++ blog/views.py
@@ -14,6 +14,7 @@ class PostListView(ListView):
   template_name = 'blog/home.html'
   context_object_name = 'posts'
   ordering = ['-date_posted']
+  paginate_by = 2
 
 def about(request):
   return render(request, 'blog/about.html', {'title': 'About'})
