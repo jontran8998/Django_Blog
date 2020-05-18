1830253d72d45ee5628c4850f6b383c8c2589d1c
Jon Tran <jontran@Jons-iMac.wp.shawcable.net>
2020-05-17 20:52:03 -0500

    Feature_update post and prevent unathorized update

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import  Post
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 def home(request):
   context = {
@@ -30,6 +30,20 @@ class PostCreateView(LoginRequiredMixin ,CreateView):
     form.instance.author = self.request.user
     return super().form_valid(form)
   
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin,UpdateView):
+  model = Post
+  fields = ['title', 'content']
+
+  def form_valid(self, form):
+    form.instance.author = self.request.user
+    return super().form_valid(form)
+
+  def test_func(self):
+    post = self.get_object()
+    if self.request.user == post.author:
+      return True
+    return False
 
 
 
